02d6b5117c0f46e986d9e5d8b2d1ad63c1c67139
AlejandroPelcas <alejandro.pelcas@berkeley.edu>
2025-01-13 09:17:02 -0800

    Get response from API

---
+++ main.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from random import randint
+import requests
+import json
+import pandas as pd
+import matplotlib as plt
+import numpy as np
+
+# Get the weather forcast:
+# First you need to use the latitude and longitude of the location you want to get the weather forcast for.
+# Next you need to use the response from the request to find the forecast url.
+# Finally you can use the forecast url to get the weather forcast.`
+longitude = -122.4194
+latitude = 37.7749
+response = requests.get(f"https://api.weather.gov/points/{latitude},{longitude}")
+
+# Get the forecast url from the response 
+forcast_url = response.json()['properties']['forecast']
+
+response = requests.get(forcast_url)
+print(response.json())
