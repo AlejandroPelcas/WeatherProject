e0c5c8b3104be6e7c6eab1b7e1a412a0d2bcf7ca
ale pel <alejandro.pelcas@gmail.com>
2025-03-07 09:51:27 -0800

    added login form

--- Weather/app.py
+++ Weather/app.py
@@ -1,9 +1,55 @@
 from flask import Flask, render_template, jsonify
 from main import fetch_weather, fetch_recommendation
 from flask_cors import CORS
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+
 # static_folder helps find the location of static folders such as js
 app = Flask(__name__, static_folder='static')
 CORS(app)  # This allows all origins
+
+app.secret_key = 'my_secret_key'
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(
+        min=4, max=20)], render_kw={"placeholder": "Username"})
+    
+    password = StringField(validators=[InputRequired(), Length(
+    min=4, max=20)], render_kw={"placeholder":"Password"})
+
+    submit = SubmitField("Register")
+
+    # def validate_username(self, username):
+
+    #     existing_user_name = User.query.filter_by(
+    #         username=username.data).first()
+        
+
+    #     )
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(
+        min=4, max=20)], render_kw={"placeholder": "Username"})
+    
+    password = StringField(validators=[InputRequired(), Length(
+    min=4, max=20)], render_kw={"placeholder":"Password"})
+
+    submit = SubmitField("Login")
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
+
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegisterForm()
+    return render_template('register.html')
+
+
 
 @app.route('/')
 def home():
